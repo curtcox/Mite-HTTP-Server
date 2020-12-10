b4df7aad5c270be0b35d9c6b2e8fdde8758f32c9
Curt Cox <curt@Curts-MacBook-Pro.local>
2020-12-09 21:27:42 -0600

    Eliminate unused instance variable.

--- src/main/java/mite/HTTPRequest.java
+++ src/main/java/mite/HTTPRequest.java
@@ -4,28 +4,26 @@ import java.util.*;
 
 public final class HTTPRequest {
 
-    public final String string;
-    public final String method;
+    public final String string; // The entire unparsed request string we were sent
+    public final String method; // GET, POST, etc...
     public final String filename;
     public final HTTPVersion httpVersion;
-    public final Exception error;
 
     public static HTTPRequest parse(String string) {
         try {
             StringTokenizer tokenizer = new StringTokenizer(string);
-            String method = tokenizer.nextToken();
+            String   method = tokenizer.nextToken();
             String filename = tokenizer.nextToken();
-            return new HTTPRequest(string, method, filename, HTTPVersion.fromRequest(string),null);
+            return new HTTPRequest(string, method, filename, HTTPVersion.fromRequest(string));
         } catch (Exception e) {
-            return new HTTPRequest(string,null,null,HTTPVersion.Unknown,e);
+            return new HTTPRequest(string,null,null,HTTPVersion.Unknown);
         }
     }
 
-    private HTTPRequest(String string, String method, String filename, HTTPVersion httpVersion, Exception error) {
+    private HTTPRequest(String string, String method, String filename, HTTPVersion httpVersion) {
         this.string = string;
         this.method = method;
         this.filename = filename;
         this.httpVersion = httpVersion;
-        this.error = error;
     }
 }
